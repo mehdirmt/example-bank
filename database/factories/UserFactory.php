62caa5dfb288cfe80825ae7bab58621c2819311f
mrahmati6903 <mehdirahmati6903@gmail.com>
2023-09-07 23:47:09 +0330

    edit User model, migration and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,19 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $mobilePrefixes = [
+            '0910',
+            '0912',
+            '0916',
+            '0936',
+            '0921',
+        ];
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name'      => fake()->unique()->userName(),
+            'full_name' => fake()->name(),
+            'mobile'    => fake()->randomElement($mobilePrefixes) . fake()->unique()->numerify('#######'),
+            'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
         ];
     }
 
