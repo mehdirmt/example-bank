adb4ace48228c41fa3424bcf42d39c83351e6678
mrahmati6903 <mehdirahmati6903@gmail.com>
2023-09-08 00:04:04 +0330

    create Card model, migration and factory

---
+++ database/migrations/2023_09_07_202752_create_cards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->string('pan', 16)->unique();
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
